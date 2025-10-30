d439578d722070634272993eb330b050704930d2
Gabor Orban <orban.gabor@siriusaudio.hu>
2025-10-31 00:09:32 +0100

    added packages to install

--- first_install.sh
+++ first_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+sudo useradd -s /bin/bash siriusaudio_daemon
+PASSWORD=$(mkpasswd -m sha-512 siriusaudio)
+echo "siriusaudio_daemon:${PASSWORD}" | sudo chpasswd
+
 mkdir -p /var/lib/sirius-installer
 mkdir -p /var/lib/sirius-installer/.keys
-apt install python3-venv python3 -y
+apt install python3-venv python3 mkpasswd whois -y
+
 python3 -m venv /var/lib/sirius-installer/.venv
 
 source /var/lib/sirius-installer/.venv/bin/activate
