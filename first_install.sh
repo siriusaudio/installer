25100baa7b989fd49120890dec84dcb5212abf26
Gabor Orban <orban.gabor@siriusaudio.hu>
2025-11-01 22:42:00 +0100

    copy files to final folder

--- first_install.sh
+++ first_install.sh
@@ -6,7 +6,7 @@ echo "siriusaudio_daemon:${PASSWORD}" | sudo chpasswd
 
 mkdir -p /var/lib/sirius-installer
 mkdir -p /var/lib/sirius-installer/.keys
-apt install python3-venv python3 mkpasswd whois -y
+apt install python3-venv python3 whois -y
 
 python3 -m venv /var/lib/sirius-installer/.venv
 
@@ -16,7 +16,9 @@ source ./authentication_key.txt
 ./client.py register $AUTH_KEY 217.144.56.118
 
 # Copy authenticate.sh and give execute permissions
-cp ./authenticate.sh /var/lib/sirius-installer/authenticate.sh
+cp ./authenticate.sh /var/lib/sirius-installer/
+cp ./client.py /var/lib/sirius-installer/
+cp ./authentication_key.txt /var/lib/sirius-installer/
 chmod +x /var/lib/sirius-installer/authenticate.sh
 
 # Copy systemd service file
