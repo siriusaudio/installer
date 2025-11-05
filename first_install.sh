5ef73df45c20236441f367ec69f6ea1030066124
Gabor Orban <orban.gabor@siriusaudio.hu>
2025-11-05 09:58:56 +0100

    modified permissions

--- first_install.sh
+++ first_install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
 sudo apt install python3-venv python3 whois -y
 
 sudo useradd -s /bin/bash siriusaudio_daemon
 PASSWORD=$(mkpasswd -m sha-512 siriusaudio)
 echo "siriusaudio_daemon:${PASSWORD}" | sudo chpasswd
 
-mkdir -p /var/lib/sirius-installer
-mkdir -p /var/lib/sirius-installer/.keys
+sudo usermod -aG sudo siriusaudio_daemon
+sudo echo "siriusaudio_daemon ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/dont-prompt-siriusaudio_daemon-for-sudo-password"
 
-chown -R siriusaudio_daemon /var/lib/sirius-installer
+sudo su siriusaudio_daemon
+
+sudo mkdir -p /var/lib/sirius-installer
+sudo mkdir -p /var/lib/sirius-installer/.keys
+
+sudo chown -R siriusaudio_daemon /var/lib/sirius-installer
+
 python3 -m venv /var/lib/sirius-installer/.venv
 
 source /var/lib/sirius-installer/.venv/bin/activate
@@ -24,7 +33,7 @@ cp ./authentication_key.txt /var/lib/sirius-installer/
 chmod +x /var/lib/sirius-installer/run_update.sh
 
 # Copy systemd service file
-cp ./sirius-authenticate.service /lib/systemd/system/sirius-authenticate.service
+sudo cp ./sirius-authenticate.service /lib/systemd/system/sirius-authenticate.service
 
 pushd /var/lib/sirius-installer/
 sudo /var/lib/sirius-installer/run_update.sh
@@ -34,6 +43,6 @@ popd
 sudo systemctl enable sirius-authenticate.service
 
 # Add cron job to run update script daily at 3am
-(crontab -l 2>/dev/null; echo "0 3 * * * /var/lib/sirius-installer/run_update.sh") | crontab -
+sudo (crontab -l 2>/dev/null; echo "0 3 * * * /var/lib/sirius-installer/run_update.sh") | crontab -
 
 
